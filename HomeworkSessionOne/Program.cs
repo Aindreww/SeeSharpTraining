7dbcd39bea35edb4deb3dad71b41d1118f04f259
Aindreww <Aindrew21@gmail.com>
2023-04-12 17:41:30 +0300

    Initial commit

---
+++ HomeworkSessionOne/Program.cs
@@ -0,0 +1,19 @@
+﻿int BoxA, BoxB, BoxC, BoxD, BoxE, BoxF, BoxG;
+BoxA = 34;
+BoxB = 4;
+BoxC = BoxA + BoxB;
+BoxD = BoxA - BoxB;
+BoxE = BoxA * BoxB;
+BoxF = BoxA / BoxB;
+BoxG = BoxA % BoxB;
+
+// Suma
+Console.WriteLine("Suma dintre BoxA si BoxB este = " + BoxC);
+// Scaderea
+Console.WriteLine("Scaderea dintre BoxA si BoxB este = " + BoxD);
+// Inmultirea
+Console.WriteLine("Inmultirea dintre BoxA si BoxB este = " + BoxE);
+// Impartirea
+Console.WriteLine("Impartirea dintre BoxA si BoxB este = " + BoxF);
+// Restul
+Console.WriteLine("Restul dintre BoxA si BoxB este = " + BoxG);
